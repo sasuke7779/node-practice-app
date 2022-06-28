5142d5f688c971211f6766025ae792a96a036282
sasuke7779 <sasuke7779@gmail.com>
2022-06-28 21:12:44 +0900

    fix dir

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const PORT = process.env.PORT;
+console.log(process.env);
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
+app.set('view engine', 'ejs');
+
+app.use('/', require('./routes/index.js'));
 
 app.listen(PORT, () => {
     console.log(PORT);
