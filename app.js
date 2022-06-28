219d0c5d2f7818781eb54770ffa60db82bb74f53
sasuke7779 <sasuke7779@gmail.com>
2022-06-28 21:34:04 +0900

    add public file

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const PORT = process.env.PORT;
-console.log(process.env);
+const path = require('path');
 const express = require('express');
+const favicon = require('serve-favicon');
 const app = express();
 
+// Express settings
 app.set('view engine', 'ejs');
 
+// Static resource rooting
+app.use(favicon(path.join(__dirname, 'public/favicon.ico')));
+app.use('/public', express.static(path.join(__dirname, '/public')));
+
+// Dynamic resource rooting
 app.use('/', require('./routes/index.js'));
 
 app.listen(PORT, () => {
